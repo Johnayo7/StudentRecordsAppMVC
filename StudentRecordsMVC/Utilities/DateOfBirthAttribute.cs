6580d0160722c53f800cc5f3a0769e643f0e3288
John Olayemi <olayemi.johnayo@gmail.com>
2024-04-11 15:21:49 +0100

    perf - implement validations on view models in the domain class and on the views to ensure that data input from the user on ther browser meets database data requirements

---
+++ StudentRecordsMVC/Utilities/DateOfBirthAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentRecordsMVC.Utilities
+{
+    public class DateOfBirthAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                DateTime dateOfBirth = (DateTime)value;
+                if (dateOfBirth > DateTime.Today)
+                {
+                    return new ValidationResult("Date of Birth cannot be in the Future");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
